5d639f518f5be3921318e89f89372568ae7e1ad4
Edward Silverton <e.silverton@gmail.com>
2021-05-18 10:36:35 +0100

    controller

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,80 +1,100 @@
-import { useControls } from 'leva'
+import { useController, useForm } from "react-hook-form";
 
-function TargetColor() {
-  const { targetColor } = useControls({
-    targetColor: {
-      options: ["red", "blue", "green"],
-      value: "red"
-    },
-  })
-  return <>{ targetColor }</>
-}
-
-function TargetType() {
-  const { targetType } = useControls({
-    targetType: {
-      options: ["cone", "cube", "sphere"],
-      value: "cone"
-    }
-  })
-  return (
-    <div>
-      <pre>{JSON.stringify(targetType, null, '  ')}</pre>
-    </div>
-  )
-}
-
-function TargetSound() {
-  const { targetSound } = useControls({
-    targetSound: {
-      options: ["beep", "boop"],
-      value: "beep"
-    }
-  })
-  return (
-    <div>
-      <pre>{JSON.stringify(targetSound, null, '  ')}</pre>
-    </div>
-  )
-}
-
-function DistractorsCount() {
-  const { distractorsCount } = useControls({ 
-    distractorsCount: {
-      min: 0,
-      max: 50,
-      step: 1,
-      value: 0,
-    }
-  })
-  return <>{distractorsCount}</>
-}
-
-function DistractorTypes() {
-  const { distractorTypes } = useControls({
-    distractorTypes: {
-      min: 1,
-      max: 3,
-      step: 1,
-      value: 1,
-    },
-  })
+function NumberInput(props) {
+  const { field, fieldState } = useController(props);
 
   return (
-    <div>
-      <pre>{JSON.stringify(distractorTypes, null, '  ')}</pre>
+    <label className="block mt-8">
+      <span className="text-gray-700">{ props.label }</span>
+      <input {...field} type="number" min={props.min} max={props.max} className="form-input mt-1 block w-full" />
+      {/* <p>{fieldState.invalid ? "invalid" : "valid"}</p> */}
+    </label>
+  );
+}
+
+function SelectInput(props) {
+  const { field, fieldState } = useController(props);
+
+  return (
+    <label className="block mt-8">
+      <span className="text-gray-700">{ props.label }</span>
+      <select {...field} className="block w-full mt-1">
+        {
+          props.options.map((option, idx) => {
+            return <option key={idx}>{option}</option>
+          })
+        }
+      </select>
+    </label>
+  );
+}
+
+function CheckboxListInput(props) {
+  const { field, fieldState } = useController(props);
+
+  return (
+    <div className="block mt-8">
+      <span className="text-gray-700">{ props.label }</span>
+      <div className="mt-2">
+        {
+          props.options.map((option, idx) => {
+            return <div key={idx}>
+              <label className="inline-flex items-center">
+                <input className="form-checkbox" {...field} type="checkbox" name="sameName" />
+                <span className="ml-2">{ option }</span>
+              </label>
+            </div>
+          })
+        }
+      </div>
     </div>
-  )
+  );
 }
 
 export default function Home() {
+
+  const primitives = ["Cone", "Cube", "Sphere"];
+  const colors = ["Red", "Green", "Blue"];
+  const targetSounds = ["Beep", "Boop"];
+
+  const { register, handleSubmit, control, watch, formState: { errors } } = useForm({
+    defaultValues: {
+      TargetColor: colors[0],
+      TargetType: primitives[0],
+      TargetSound: targetSounds[0],
+      DistractorCount: 1
+    },
+    mode: "onChange"
+  });
+  const onSubmit = data => console.log(data);
+
+  // console.log(watch("title")); // watch input value by passing the name of it
+
   return (
-    <>
-      <TargetColor />
-      <TargetType />
-      <TargetSound /> 
-      <DistractorsCount />
-      <DistractorTypes /> 
-    </>
-  )
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)} className="p-8 max-w-screen-md">
+
+      {/* <label className="block mt-8">
+        <span className="text-gray-700">Title</span>
+        <input type="text" className="mt-1 block w-full" defaultValue="test" {...register("title")} />
+      </label> */}
+
+      <SelectInput control={control} options={colors} label="Target Colour" name="TargetColor" rules={{ required: true }}  />
+
+      <SelectInput control={control} options={primitives} label="Target Type" name="TargetType" rules={{ required: true }}  />
+
+      <SelectInput control={control} options={targetSounds} label="Target Sound" name="TargetSound" rules={{ required: true }}  />
+
+      <NumberInput control={control} label="Distractor Count" min={1} max={50} name="DistractorCount" rules={{ required: true }}  />
+
+      <CheckboxListInput control={control} options={primitives} label="Distractor Types" name="DistractorTypes" rules={{ required: true }}  />
+
+      {/* <input type="text" {...register("exampleRequired", { required: true })} />
+      {errors.exampleRequired && <span>This field is required</span>} */}
+      
+      <div className="block mt-8">
+        <button type="submit" className="p-4 font-medium bg-gray-200">Download Config</button>
+      </div>
+    </form>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -318,7 +318,7 @@ module.exports = {
         '"Segoe UI Symbol"',
         '"Noto Color Emoji"',
       ],
-      serif: ['Merriweather', 'ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
       mono: [
         'ui-monospace',
         'SFMono-Regular',
@@ -991,6 +991,8 @@ module.exports = {
     wordBreak: ['responsive'],
     zIndex: ['responsive', 'focus-within', 'focus'],
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
